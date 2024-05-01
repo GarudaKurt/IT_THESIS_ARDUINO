dc9fa6f1687249ce81818b0b47c10d9de59da631
Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2024-05-01 11:16:07 +0800

    arduino barcode reader

--- mycode.ino
+++ mycode.ino
@@ -1,16 +1,14 @@
 #include <SoftwareSerial.h>
 
 // Define the pins for SoftwareSerial
-const int barcodeReaderRxPin = D1; // Connect the barcode reader TX pin to this pin
-const int barcodeReaderTxPin = D2; // Connect the barcode reader RX pin to this pin
+const int barcodeReaderRxPin = 7; // Connect the barcode reader TX pin to this pin
+const int barcodeReaderTxPin = 8; // Connect the barcode reader RX pin to this pin
 
 // Create a SoftwareSerial object
 SoftwareSerial barcodeReader(barcodeReaderRxPin, barcodeReaderTxPin);
 
 void setup() {
-  // Initialize Serial communication for debugging
   Serial.begin(9600);
-  
   // Initialize SoftwareSerial for communication with the barcode reader
   barcodeReader.begin(9600);
 }
